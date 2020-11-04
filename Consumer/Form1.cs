24bb0c918ab98030fe6b8dcb00a47d3df3bd0f78
ajugalushkin <ajugalushkin@gmail.com>
2020-11-04 18:34:48 +0300

    Add producer.

--- Consumer/Form1.cs
+++ Consumer/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,7 +23,28 @@ namespace Consumer
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            //var port = int.Parse(ConfigurationManager.AppSettings.Get("port")); 
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            var client = new UdpClient(port);
+
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+
+                using (var ms = new MemoryStream(data.Buffer))
+                {
+                    pictureBox1.Image = new Bitmap(ms);
+                }
+
+                Text = $"Bytes received: { data.Buffer.Length * sizeof(byte) }";
+            }
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            MessageBox.Show(string.Join( "\n", host.AddressList.
+                Where(i => i.AddressFamily == AddressFamily.InterNetwork)
+                .Select(i=> i.ToString())));
         }
     }
 }
